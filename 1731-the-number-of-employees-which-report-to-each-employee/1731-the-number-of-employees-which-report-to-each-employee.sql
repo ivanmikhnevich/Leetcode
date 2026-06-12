15afe9ffe475ed9635ee611256a2997be0a8fea1
Ivan <ivanmichnevih@gmail.com>
2026-06-12 22:37:21 +0300

    Delete 1731-the-number-of-employees-which-report-to-each-employee directory

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    m.employee_id,
-    m.name,
-    COUNT(e.reports_to) AS reports_count,
-    ROUND(AVG(e.age)) AS average_age
-FROM Employees m
-JOIN Employees e ON m.employee_id = e.reports_to
-GROUP BY m.employee_id, m.name
-ORDER BY employee_id
