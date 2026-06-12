2c594bba5e1b9ddb9ccb8935da8ebe2aa30c51cd
Ivan <ivanmichnevih@gmail.com>
2026-06-12 22:35:12 +0300

    Delete 1633-percentage-of-users-attended-a-contest directory

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    r.contest_id,
-    ROUND(COUNT(r.user_id)::numeric*100/ (SELECT COUNT(*)::numeric FROM Users), 2) AS percentage
-FROM Register r
-JOIN  Users u ON r.user_id = u.user_id
-GROUP BY r.contest_id
-ORDER BY percentage  DESC, r.contest_id Asc;
